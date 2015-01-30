9d226ffe14dae48f6eaef5ca760e63652a5b8c60
Stanley Tang <stang05@csclnx02.ad.calpoly.edu>
2015-01-29 22:28:24 -0800

    Lab 4

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,6 @@
 "use script"
 window.onload = getTime();
+window.onload = getTemp();
 function getTime() {
   var clockDivElement = document.getElementById("clock");
   var d = new Date();
@@ -7,4 +8,36 @@ function getTime() {
   	d.getMinutes() + ": " + d.getSeconds();
 }
 
+function getTemp() {
+  
+  $.ajax({
+    url: "https://api.forecast.io/forecast/532b8d8cb00fde6b2cd56ab58fee0a43/35.300399,-120.662362?callback=?",
+    type: "GET",
+	dataType: "json",
+	success: function(data) {
+	
+		console.log("I WAS SUCCESSFUL!")
+		console.log(data.daily.icon)
+	
+		var temp = data.daily.data[0].temperatureMax
+		$("#forecastLabel").html(data.daily.summary)
+		$("#forecastIcon").attr("src", "img/" + data.daily.icon + ".png")
+		
+		var dynamicTempClass;
+		if (temp >= 90) {
+			dynamicTempClass = "coldclass";
+		} else if (temp >= 80) {
+			dynamicTempClass = "warmclass";
+		} else if (temp >= 70) {
+			dynamicTempClass = "niceclass";
+		} else if (temp >= 60) {
+			dynamicTempClass = "chillyclass";
+		} else {
+			dynamicTempClass = "coldclass";
+		}
+		
+		$("body").attr("class", dynamicTempClass);
+	}
+  })
+}
 setInterval(getTime, 100);
