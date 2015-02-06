faee2fab65be05808695bf72de7c3eb844d30f88
Stanley Tang <stang05@csclnx02.ad.calpoly.edu>
2015-02-06 03:20:03 -0800

    Lab 5 alarms

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,8 @@
 "use script"
 window.onload = getTime();
 window.onload = getTemp();
+window.onload = getAllAlarms();
+
 function getTime() {
   var clockDivElement = document.getElementById("clock");
   var d = new Date();
@@ -16,8 +18,6 @@ function getTemp() {
 	dataType: "json",
 	success: function(data) {
 	
-		console.log("I WAS SUCCESSFUL!")
-		console.log(data.daily.icon)
 	
 		var temp = data.daily.data[0].temperatureMax
 		$("#forecastLabel").html(data.daily.summary)
@@ -40,4 +40,104 @@ function getTemp() {
 	}
   })
 }
+
+function showAlarmPopup() {
+	$("#mask").removeClass("hide");
+	$("#popup").removeClass("hide");	
+}
+
+function hideAlarmPopup() {
+	$("#mask").addClass("hide");
+	$("#popup").addClass("hide");	
+}
+
+
+
+function insertAlarm(hours, mins, ampm, alarmName) {
+	var div = $("<div>");
+	var name = $("<div>");
+	var time = $("<div>");
+	
+	$(div).addClass(".flexable");
+	
+	name.addClass("name");
+	name.html(alarmName);
+	name.appendTo(div);
+	
+	time.addClass("time");
+	time.html("" + hours + ":" + mins+ " " + ampm);
+	
+	$(div).append(name);
+	$(div).append(time);
+	
+	$("#alarms").append(div);
+	
+	var deleteButton = $("<input type=\"button\" value=\"Delete\">");
+	deleteButton.click(function() {
+		deleteAlarm(hours, mins, ampm, alarmName);
+	});
+	
+	$("#alarms").append(deleteButton);	
+}
+
+function addAlarm() {
+	var hours = $("#hours option:selected").text();
+	var mins = $("#mins option:selected").text();
+	var ampm = $("#ampm option:selected").text();
+	var alarmName = $("#alarmName").val();
+	
+	
+	var AlarmObject = Parse.Object.extend("Alarm");
+    var alarmObject = new AlarmObject();
+	alarmObject.save({"hours": hours, "mins": mins, "ampm": ampm, "alarmName": alarmName}, {
+		success: function(object) {
+			insertAlarm(hours, mins, ampm, alarmName);
+			hideAlarmPopup();
+		}
+    });
+}
+
+function deleteAlarm(hours, mins, ampm, alarmName) {
+	var AlarmObject = Parse.Object.extend("Alarm");
+	var query = new Parse.Query(AlarmObject);
+	
+	query.equalTo("hours", hours);
+	query.equalTo("mins", mins);
+	query.equalTo("ampm", ampm);
+	query.equalTo("alarmName", alarmName);
+	
+	query.find({
+		success: function(results) {
+			var toDelete = results[0];
+			toDelete.destroy({
+				success: function(toDelete) {				
+					getAllAlarms(); 
+				}
+			});
+		}
+	});
+	
+	$("#" + this.document.activeElement.alarmName).parent().parent().remove(); 
+}
+
+function getAllAlarms() {
+	$("#alarms").html("");
+	Parse.initialize("EKs3E7ICtmGoA5G8gLv9WJrYIsSnwwD1WTnMh9bZ", "mviQyXcG2mPvGlICGcfIMfb3eAVVeelqwvGP8OnS");
+
+	var AlarmObject = Parse.Object.extend("Alarm");
+	var query = new Parse.Query(AlarmObject);
+	query.find({
+		success: function(results) {
+			console.log(results);
+			
+			for (var i = 0; i < results.length; i++) { 
+				insertAlarm(results[i].get("hours"),
+							results[i].get("mins"),
+							results[i].get("ampm"),
+							results[i].get("alarmName"));
+			}
+		}
+	});
+}
+
 setInterval(getTime, 100);
